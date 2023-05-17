a279e14b34875aac0c69338008bd37bb38ac40e7
term117 <112412747+term117@users.noreply.github.com>
2023-05-17 19:18:15 -0400

    Create Chapter 2 - Variables and Simple Data Types.py

---
+++ Chapter 2 - Variables and Simple Data Types.py
@@ -0,0 +1,24 @@
+# Chapter 2 - Variables and Simple Data Types 5/17/2023
+bestGame = "Skyrim"
+print(bestGame)
+bestGame = "Valhiem"
+print(bestGame)
+
+name = "Gabby"
+print("Hello",name,"how was your day?")
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+name = "Paarthurnax"
+quote = '"What is better, to be born good, or to overcome your evil nature through great effort?"'
+print(name,"once said,",quote)
+
+whiteSpace = ' Space '
+print(whiteSpace)
+print(whiteSpace.strip())
+
+print(4+4)
+print(12-4)
+print(2*4)
+print(24/3)

3d6dc68ed1ea94e48dfc35dd7bd7d6b48ac5dbb9
term117 <112412747+term117@users.noreply.github.com>
2023-05-17 19:18:54 -0400

    Create Chapter 3 - Lists.py

---
+++ Chapter 3 - Lists.py
@@ -0,0 +1,14 @@
+# Chapter 3 - Lists 5/17/2023
+names = ['zach','riley','andrew']
+print(names[0])
+print(names[1])
+print(names[2])
+message = 'Get online'
+print(message,names[0])
+print(message,names[1])
+print(message,names[2])
+
+bikes = ['Kawasaki','Susuki','Honda']
+print('I use to own a',bikes[1])
+print('I have a',bikes[0])
+print('I want to get a',bikes[2])

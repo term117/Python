3161ea7c86ebf7824575774080257183324b270b
term117 <112412747+term117@users.noreply.github.com>
2023-05-18 17:39:28 -0400

    Create Chapter 4 - Working With Lists.py

---
+++ Chapter 4 - Working With Lists.py
@@ -0,0 +1,10 @@
+# Chapter 4 - Working With Lists
+pizzas = ['mushroom pizza','ham pizza','sausage pizza']
+for pizza in pizzas:
+    print(pizza,'is my favorite kind of pizza!')
+print('I really love pizza!')
+
+pets = ['cat','dog','hamster']
+for pet in pets:
+    print('A',pet,'would make a great pet!')
+print('You can find all these animals at petsmart!')

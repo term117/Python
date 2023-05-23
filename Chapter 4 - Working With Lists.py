b17a95c232c88a59be963f68c241a55f4f1c93c3
term117 <112412747+term117@users.noreply.github.com>
2023-05-23 16:20:41 -0400

    Update Chapter 4 - Working With Lists.py

--- Chapter 4 - Working With Lists.py
+++ Chapter 4 - Working With Lists.py
@@ -12,20 +12,24 @@ print('You can find all these animals at petsmart!')
 for value in range (1,21):
     print(value)
 numbers = list(range(1,1000001))
-print(numbers)
+for number in numbers:
+    print(number)
 print(min(numbers))
 print(max(numbers))
 
 odd_numbers = (list(range(1,20,2)))
-print(odd_numbers)
+for odd_number in odd_numbers:
+    print(odd_number)
 
 multi_3 = (list(range(3,31,3)))
-print(multi_3)
+for multi in multi_3:
+    print(multi)
 
 cubed = [value**2 for value in range(1,11)]
-print(cubed)
+for cube in cubed:
+    print(cube)
 
-buddies = ["riley", "zach", "andrew", "ben", "micheal", "jake"]
+buddies = ["riley", "zach", "andrew", "ben", "michael", "jake"]
 print("The first three names are:",buddies[:3])
 print("Three names from the middle of the list are:",buddies[1:4])
 print("The the three names from the list are:",buddies[3:6])
@@ -35,3 +39,10 @@ pizzas.append("pepperoni pizza")
 friend_pizzas.append("pineapple pizza")
 print("My favorite pizzas are:",pizzas)
 print("My friends favorites pizzas are:",friend_pizzas)
+
+buffet_foods = ("Chicken","Waffles","Eggs","Bacon","Sausage")
+for buffet_food in buffet_foods:
+    print(buffet_food)
+buffet_foods_revised = ("Chicken","Waffles","Eggs","Yogurt","Pastry")
+for buffet_foods_revise in buffet_foods_revised:
+    print(buffet_foods_revise)

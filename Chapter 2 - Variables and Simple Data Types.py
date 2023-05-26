549ce6e8f8d6d971d02d42e20053e6658871ff02
term117 <112412747+term117@users.noreply.github.com>
2023-05-26 16:21:06 -0400

    Update Chapter 2 - Variables and Simple Data Types.py

--- Chapter 2 - Variables and Simple Data Types.py
+++ Chapter 2 - Variables and Simple Data Types.py
@@ -1,27 +1,33 @@
 # Chapter 2 Variables and Simple Data Types
+
+# 2-1, 2-2 Simple Messages
 bestGame = "Skyrim"
 print(bestGame)
 bestGame = "Valhiem"
 print(bestGame)
 
+# 2-3, 2-3 Personal Message and Name Cases
 name = "Gabby"
 print("\nHello", name, "how was your day?")
 print(name.lower())
 print(name.upper())
 print(name.title())
 
+# 2-5 Famous Quote
 name = "Paarthurnax"
 quote = '"What is better, to be born good, or to overcome your evil nature through great effort?"'
 print(name, "once said,", quote)
 
 print('')
 
+# 2-7 Stripping Names
 whiteSpace = ' Space '
 print(whiteSpace)
 print(whiteSpace.strip())
 
 print('')
 
+# 2-8 Number Eight
 print(4 + 4)
 print(12 - 4)
 print(2 * 4)

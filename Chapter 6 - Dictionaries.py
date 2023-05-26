7c5e9923e363a83d866facd0f388c9dd6e31fd7e
term117 <112412747+term117@users.noreply.github.com>
2023-05-25 21:44:50 -0400

    Create Chapter 6 - Dictionaries.py

---
+++ Chapter 6 - Dictionaries.py
@@ -0,0 +1,8 @@
+# Chapter 6 - Dictionaries
+gabby = {'first_name': 'gabby','last_name': 'rogers','age': '24','city': 'essex'}
+print(gabby['first_name'].title())
+print(gabby['last_name'].title())
+print(gabby['age'])
+print(gabby['city'].title())
+
+fav_numbers = {'zach': '1','riley': '2','andrew': '3','chuck': '4','gabby': '5'}

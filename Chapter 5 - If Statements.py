4cf077f9140d7cea70b26833054637a949afaf89
term117 <112412747+term117@users.noreply.github.com>
2023-05-26 16:21:12 -0400

    Update Chapter 5 - If Statements.py

--- Chapter 5 - If Statements.py
+++ Chapter 5 - If Statements.py
@@ -1,4 +1,6 @@
 # Chapter 5 - If Statements
+
+# 5-1, 5-2 Conditional Tests
 motorcycle = "zx6r"
 print("Is motorcycle == 'zx6r'? Yes")
 print(motorcycle == 'zx6r')
@@ -10,7 +12,7 @@ game = 'Valhiem'
 print(game.lower() == 'valhiem')
 
 num = 26
-if num <=11 or num >=13:
+if num <= 11 or num >= 13:
     print('\nThe number is not 12')
 
 age = 32
@@ -19,16 +21,17 @@ if age > 18 and age >= 21:
 
 print('')
 
-coding_langs = ['python','C#','Java']
+coding_langs = ['python', 'C#', 'Java']
 lang = 'python'
 if lang in coding_langs:
-    print(lang,"is a conding language")
+    print(lang, "is a conding language")
 lang = 'MSI'
 if lang not in coding_langs:
-    print(lang,'is not a coding language')
+    print(lang, 'is not a coding language')
 
 print('')
 
+# 5-3, 5-4, 5-5 Alien Colors
 alien_color = 'yellow'
 if alien_color == 'green':
     print('+5 points')
@@ -39,6 +42,7 @@ elif alien_color == 'red':
 
 print('')
 
+# 5-6 Stages of Life
 age = 32
 if age < 2:
     print('This person is a baby')
@@ -55,7 +59,8 @@ elif age >= 65:
 
 print('')
 
-fruits = ['black berry','mango','strawberry','watermelon']
+# Favorite Fruit
+fruits = ['black berry', 'mango', 'strawberry', 'watermelon']
 if 'mango' in fruits:
     print('You really like mangoes')
 if 'black berry' in fruits:
@@ -63,7 +68,8 @@ if 'black berry' in fruits:
 
 print('')
 
-user_name = ['king','term','cosmic','ribread','admin']
+# 5-8, 5-9 Hello Admin
+user_name = ['king', 'term', 'cosmic', 'ribread', 'admin']
 if 'king' in user_name:
     print('Hello king')
 if 'term' in user_name:
@@ -74,31 +80,38 @@ if 'ribread' in user_name:
     print('Hello ribread')
 if 'admin' in user_name:
     print('Hello admin, would you like to see a status report?')
-else: print('We need to find some user!')
+else:
+    print('We need to find some user!')
 
 print('')
 
-current_users = ['king','term','cosmic','ribread']
-new_users = ['term','ribread','kitty','anna']
+# 5-10 Checking for Usernames
+current_users = ['king', 'term', 'cosmic', 'ribread']
+new_users = ['term', 'ribread', 'kitty', 'anna']
 if 'term' in current_users:
     print('You will need to enter a new user name')
-else: print('This username is available')
+else:
+    print('This username is available')
 
 if 'ribread' in current_users:
     print('You will need to enter a new user name')
-else: print('This username is available')
+else:
+    print('This username is available')
 
 if 'kitty' in current_users:
     print('You will need to enter a new user name')
-else: print('This username is available')
+else:
+    print('This username is available')
 
 if 'anna' in current_users:
     print('You will need to enter a new user name')
-else: print('This username is available')
+else:
+    print('This username is available')
 
 print('')
 
-nums = list(range(1,10))
+# 5-11 Ordinal Numbers
+nums = list(range(1, 10))
 for num in nums:
     if num == 1:
         print('1st')
@@ -106,4 +119,5 @@ for num in nums:
         print('2nd')
     elif num == 3:
         print('3rd')
-    else: print(f'{num}th')
+    else:
+        print(f'{num}th')

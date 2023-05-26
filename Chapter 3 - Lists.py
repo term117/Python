b84c6b7239e3369500790383b0b71b244568d1e5
term117 <112412747+term117@users.noreply.github.com>
2023-05-26 16:21:08 -0400

    Update Chapter 3 - Lists.py

--- Chapter 3 - Lists.py
+++ Chapter 3 - Lists.py
@@ -1,36 +1,41 @@
 # Chapter 3 - Lists
-names = ['zach','riley','andrew']
+
+# 3-1, 3-2 Names and Greetings
+names = ['zach', 'riley', 'andrew']
 print(names[0])
 print(names[1])
 print(names[2])
 message = 'Get online'
-print(message,names[0])
-print(message,names[1])
-print(message,names[2])
+print(message, names[0])
+print(message, names[1])
+print(message, names[2])
 
-bikes = ['Kawasaki','Susuki','Honda']
-print('\nI use to own a',bikes[1])
-print('I have a',bikes[0])
-print('I want to get a',bikes[2])
+# 3-3 Your Own Lists
+bikes = ['Kawasaki', 'Suzuki', 'Honda']
+print('\nI use to own a', bikes[1])
+print('I have a', bikes[0])
+print('I want to get a', bikes[2])
 
 print('')
 
-guestList = ['Neil Armstrong,','Neil Degrasse Tyson,','Galileo Galilei,']
+# 3-4, 3-5 Guest List and Changing Guest List
+guestList = ['Neil Armstrong,', 'Neil Degrasse Tyson,', 'Galileo Galilei,']
 message = 'I would like to formal invite you for dinner tonight.'
-print(guestList[0],message)
-print(guestList[1],message)
-print(guestList[2],message)
-print(guestList[1],'will not be attending tonight.')
+print(guestList[0], message)
+print(guestList[1], message)
+print(guestList[2], message)
+print(guestList[1], 'will not be attending tonight.')
 del guestList[1]
 guestList.append('Kitboga,')
-print(guestList[0],message)
-print(guestList[1],message)
-print(guestList[2],message)
+print(guestList[0], message)
+print(guestList[1], message)
+print(guestList[2], message)
 print(len(guestList))
 
 print('')
 
-places = ['tokyo','hawaii','jamaica','germany','greece']
+# 3-8 Seeing the World
+places = ['tokyo', 'hawaii', 'jamaica', 'germany', 'greece']
 print(places)
 print(sorted(places))
 print(places)
